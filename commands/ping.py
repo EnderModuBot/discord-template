8187407f18df37699650a1a2adcb5852c307011c
Endkind <endkind.ender@gmail.com>
2025-06-20 14:22:47 +0200

    add example commands for permission checks and update ping command response method

--- commands/ping.py
+++ commands/ping.py
@@ -1,10 +1,10 @@
 from discord import Interaction
 from ModuBotDiscord import ModuBotDiscord
-from ModuBotDiscord.commands import BaseCommand
+from ModuBotDiscord.commands import BaseCommand, send_message
 
 
 class PingCommand(BaseCommand):
     async def register(self, bot: ModuBotDiscord):
         @bot.tree.command(name="ping", description="Responds with Pong!")
         async def ping(interaction: Interaction):
-            await interaction.response.send_message("Pong!", ephemeral=True)
+            await send_message(interaction, "Pong!", True)
